4b4995d79309552d86a872808fa74b01e9ddda51
endnef2 <82787428+endnef2@users.noreply.github.com>
2023-07-08 20:03:39 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 56 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
@@ -1,12 +1,11 @@
-x, y = [], []
-n, m = map(int, input().split())
-for i in range(n):
-    x.append(list(map(int, input().split())))
-
-for i in range(n):
-    y.append(list(map(int, input().split())))
-
-for i in range(n):
-    for j in range(m):
-        print(x[i][j]+y[i][j], end=' ')
+N, M = map(int, input().split())
+A = []
+B = []
+for i in range(N):
+    A.append(list(map(int, input().split())))
+for i in range(N):
+    B.append(list(map(int, input().split())))
+for i in range(N):
+    for j in range(M):
+        print(A[i][j] + B[i][j], end = ' ')
     print()
