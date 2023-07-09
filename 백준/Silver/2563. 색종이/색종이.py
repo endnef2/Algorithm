1b02df445f479070a8a62be08db2b030a676140e
endnef2 <82787428+endnef2@users.noreply.github.com>
2023-07-09 23:51:00 +0900

    [Silver V] Title: 색종이, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.py
+++ 백준/Silver/2563. 색종이/색종이.py
@@ -1,17 +1,11 @@
-N = int(input())
-A = []
-B = []
-for i in range(N):
-    x, y = map(int, input().split())
-    A.append(x)
-    B.append(y)
-C = [[0 for i in range(max(B)+10)] for i in range(max(A)+10)]
-for k in range(N):
-    for i in range(A[k],A[k]+10):
-        for j in range(B[k],B[k]+10):
-            C[i][j] = 1
+n = int(input())
+arr = [[0 for _ in range(101)] for _ in range(101)]
+for i in range(n):
+    a, b = map(int, input().split())
+    for i in range(a, a+10):
+        for j in range(b, b+10):
+            arr[i][j]=1
 total = 0
-for i in range(len(C)):
-    for j in range(len(C[0])):
-        total+=C[i][j]
+for i in arr:
+    total+=i.count(1)
 print(total)
