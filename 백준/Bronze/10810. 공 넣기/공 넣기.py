291d4914be52b6be89ebc67b8d2b2012872c3e35
endnef2 <82787428+endnef2@users.noreply.github.com>
2023-09-12 22:19:16 +0900

    [Bronze III] Title: 공 넣기, Time: 124 ms, Memory: 114328 KB -BaekjoonHub

--- 백준/Bronze/10810. 공 넣기/공 넣기.py
+++ 백준/Bronze/10810. 공 넣기/공 넣기.py
@@ -1,7 +1,7 @@
-N, M = map(int, input().split())
-lst1 = [0]*N
-for _ in range(M):
+n, m = map(int, input().split())
+lst1 = [0]*n
+for _ in range(m):
     i, j, k = map(int, input().split())
-    for idx in range(i-1, j):
-        lst1[idx] = k
+    for l in range(i-1, j):
+        lst1[l] = k
 print(*lst1)
