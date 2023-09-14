5ce2d3085d1d78488d21c83203f7e926a5182dd3
endnef2 <82787428+endnef2@users.noreply.github.com>
2023-09-14 19:51:36 +0900

    [Bronze II] Title: 공 바꾸기, Time: 116 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
@@ -1,6 +1,6 @@
-N, M = map(int, input().split())
-lst1 = [i for i in range(1, N+1)]
-for _ in range(M):
+n, m = map(int, input().split())
+lst1 = list(range(0, n+1))
+for _ in range(m):
     i, j = map(int, input().split())
-    lst1[i-1], lst1[j-1] = lst1[j-1], lst1[i-1]
-print(*lst1)
+    lst1[i], lst1[j] = lst1[j], lst1[i]
+print(*lst1[1:], end=' ')
